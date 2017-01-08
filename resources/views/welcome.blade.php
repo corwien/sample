d8734a0abf0d392c8187cd836786e2a3d67654df
Corwien <corwien@126.com>
2017-01-08 14:58:12 +0800

    test github

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Hey, Laravel 5</div>
+                <div class="title">Hey, Laravel 5 !</div>
             </div>
         </div>
     </body>
